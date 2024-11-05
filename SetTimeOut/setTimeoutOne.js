07ce855c2c951939bd18a8e31040780835aa40e4
Aniqa1127 <aniqas897@gmail.com>
2024-11-05 11:53:02 -0500

    Create setTimeoutOne.js

---
+++ SetTimeOut/setTimeoutOne.js
@@ -0,0 +1,6 @@
+"use strict";
+console.log("Starting of Anonymous Function");
+setTimeout(function callBack(){
+    console.log("Hi, I am Call Back Function!");
+},6000);
+console.log("End of Anonymous Function");
